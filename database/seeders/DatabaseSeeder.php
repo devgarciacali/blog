5b02873958ff566a11f28b0325dba21effd8cb41
Jose Manuel Garcia Calixto <garciacalimanegarcia@gmail.com>
2025-11-26 17:47:53 -0600

    cambios en diferentes partes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,12 +11,8 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    {   
+        //llamar al sedeer
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       User::create([
+           'full_name' => 'Manuel Calixto',
+           'email' => 'admin@admin.com',
+           'password' => Hash::make('12345678'),
+       ]);
+
+       User::create([
+           'full_name' => 'maria',
+           'email' => 'jose@gmail.com',
+           'password' => Hash::make('12345678'),
+       ]);
+
+       User::factory(10)->create();
+    }
+}
