a7281663c7a2d8de3ceb32a1b9360bddc8490a76
Jose Manuel Garcia Calixto <garciacalimanegarcia@gmail.com>
2025-11-27 00:14:37 -0600

    se agregan los nuevos factorie de comentarios y usuarios

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,11 @@ class DatabaseSeeder extends Seeder
     {   
         //llamar al sedeer
         $this->call(UserSeeder::class);
+        //factory de categorias
+        Category::factory(8)->create();
+        //factory de articulos
+        Article::factory(20)->create();
+        //factory de comentarios
+        Comment::factory(20)->create();
     }
 }
